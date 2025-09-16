2e26e4a9d6ff612a98aeecd05a9465a024404d41
Luan <luanpa082@gmail.com>
2025-09-16 14:41:23 -0300

    Corrigido start.sh com URL MongoDB codificada

--- start.sh
+++ start.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-# GenieACS start script for Render.com
 
-# Configure MongoDB URL
-export MONGO_URL="mongodb+srv://luanpa082_db_user:L1an1010@cluster0.n3wzlxp.mongodb.net/genieacs?retryWrites=true&w=majority"
-
-# Configure Redis URL if needed
+# URLs do MongoDB e Redis
+export MONGO_URL="mongodb+srv://luanpa082:L1an1010%40@cluster0.n3wzlxp.mongodb.net/genieacs?retryWrites=true&w=majority"
 export REDIS_URL="redis://localhost:6379"
 
-# Start GenieACS services
+# Inicia todos os serviços do GenieACS
 node ./bin/genieacs-cwmp &
 node ./bin/genieacs-fs &
 node ./bin/genieacs-nbi &
 node ./bin/genieacs-ui &
 
-# Keep the main process alive
+# Mantém o processo principal ativo
 wait
