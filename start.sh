b6b6e5402957b4101a25d36d433fc2b096232e2d
Luan <luanpa082@gmail.com>
2025-09-16 02:42:00 -0300

    Adiciona start.sh e create-assets.js para deploy no Render

--- start.sh
+++ start.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
-# Instala dependências e build
+# Sai se houver algum erro
+set -e
+
+# Instala dependências
 npm install
+
+# Build dos binários
 npm run build
 
-# Inicia o GenieACS CWMP ligado à porta do Render
-node bin/genieacs-cwmp --port $PORT --log-level info
+# Inicia GenieACS
+# CWMP precisa abrir a porta $PORT para Render detectar
+node bin/genieacs-cwmp --port $PORT --log-level info &
+
+# FS e NBI podem rodar em background
+node bin/genieacs-fs --log-level info &
+node bin/genieacs-nbi --log-level info &
+
+# Mantém o container ativo
+wait
