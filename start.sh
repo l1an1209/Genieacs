6118f3fdb835b734217734f8821ea9bd2b21b0db
Luan <luanpa082@gmail.com>
2025-09-16 14:19:03 -0300

    Corrigido start.sh para Render

--- start.sh
+++ start.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-# GenieACS start script para Render.com / Windows Git Bash
+# GenieACS start script para Render.com
 
-# Ajusta PATH para garantir que o Node seja encontrado
-export PATH="/c/Program Files/nodejs:$PATH"
-
-# Defina suas URLs do MongoDB e Redis
-export MONGO_URL="mongodb+srv://luanpa082:L1an1010%40@cluster0.n3wzlxp.mongodb.net/genieacs?retryWrites=true&w=majority"
-export REDIS_URL="redis://127.0.0.1:6379"  # ou sua URL do Redis real
+# Não definir MONGO_URL nem REDIS_URL aqui, use variáveis de ambiente da Render
 
 # Inicia todos os serviços do GenieACS
 node ./bin/genieacs-cwmp &
