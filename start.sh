7cf1dec5dcc2f5143ae081858d004e0e7320f473
Luan <luanpa082@gmail.com>
2025-09-16 12:37:53 -0300

    Atualiza package.json e start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-# start.sh corrigido para Render.com ou Linux
+# GenieACS start script para Render.com
+
+# Defina suas URLs do MongoDB e Redis
+export MONGO_URL=mongodb+srv://luanpa082:<db_password>@cluster0.n3wzlxp.mongodb.net/
+export REDIS_URL="redis://localhost:6379" # ou sua URL do Redis
+
+# Inicia todos os serviços do GenieACS
 node ./bin/genieacs-cwmp &
 node ./bin/genieacs-fs &
 node ./bin/genieacs-nbi &
 node ./bin/genieacs-ui &
+
+# Mantém o processo principal ativo
 wait
