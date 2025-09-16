0cc189096944f36b4ba01d2a3c425fbd3256e69a
Luan <luanpa082@gmail.com>
2025-09-16 13:57:46 -0300

    Corrige start.sh com variáveis de ambiente seguras e adiciona generateAssets.js

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # GenieACS start script para Render.com
 
-# Defina suas URLs do MongoDB e Redis
-export MONGO_URL=mongodb+srv://luanpa082:<L1an1010@>@cluster0.n3wzlxp.mongodb.net/
-export REDIS_URL="redis://localhost:6379" # ou sua URL do Redis
+# Defina suas URLs do MongoDB e Redis com aspas simples
+export MONGO_URL='mongodb+srv://luanpa082_db_user:9KZLNTmKsqxDCggj@cluster0.n3wzlxp.mongodb.net/genieacs?retryWrites=true&w=majority'
+export REDIS_URL='redis://localhost:6379' # ou sua URL do Redis
 
 # Inicia todos os serviços do GenieACS
 node ./bin/genieacs-cwmp &
