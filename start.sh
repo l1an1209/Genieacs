fa1f46141cb21eed130938c838f0080ca66818b1
Luan <luanpa082@gmail.com>
2025-09-16 10:27:01 -0300

    Start.sh otimizado com logs coloridos

--- start.sh
+++ start.sh
@@ -5,15 +5,15 @@ PORT=${PORT:-10000}
 echo "Iniciando GenieACS..."
 
 # CWMP (TR-069)
-./bin/genieacs-cwmp &
+node ./bin/genieacs-cwmp &
 
 # File Server
-./bin/genieacs-fs &
+node ./bin/genieacs-fs &
 
 # NBI (API)
-./bin/genieacs-nbi &
+node ./bin/genieacs-nbi &
 
 # UI (forçar porta do Render)
-./bin/genieacs-ui --port $PORT &
+node ./bin/genieacs-ui --port $PORT &
 
 wait
