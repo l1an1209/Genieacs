c6617925f61cd7df8e1d3759629a4bcf8b590344
Luan <luanpa082@gmail.com>
2025-09-16 14:45:19 -0300

    Adiciona start.sh para deploy no Render

--- start.sh
+++ start.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+# GenieACS start script para Linux/Render
 
-# URLs do MongoDB e Redis
-export MONGO_URL="mongodb+srv://luanpa082:L1an1010%40@cluster0.n3wzlxp.mongodb.net/genieacs?retryWrites=true&w=majority"
-export REDIS_URL="redis://localhost:6379"
+# Defina suas URLs do MongoDB e Redis
+export MONGO_URL="mongodb+srv://luanpa082:L1an1010@cluster0.n3wzlxp.mongodb.net/genieacs?retryWrites=true&w=majority"
+export REDIS_URL="redis://localhost:6379" # ou sua URL do Redis
 
-# Inicia todos os serviços do GenieACS
+# Inicia todos os serviços do GenieACS em background
+node ./bin/genieacs-cwmp &
+node ./bin/genieacs-fs &
+node ./bin/genieacs-nbi &
+node ./bin/genieacs-ui &
+
+# Mantém o processo principal ativo
+wait
 node ./bin/genieacs-cwmp &
 node ./bin/genieacs-fs &
 node ./bin/genieacs-nbi &
