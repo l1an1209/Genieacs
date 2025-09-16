798082c791f9c1ac48ce037427f48570129f32c6
Luan <luanpa082@gmail.com>
2025-09-16 11:15:43 -0300

    Torna start.sh executável

--- start.sh
+++ start.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
-
-# Forçar a porta do Render ou usar 3000 por padrão
 PORT=${PORT:-3000}
 
 echo "Iniciando GenieACS..."
 
-# CWMP (TR-069)
 node ./bin/genieacs-cwmp &
-
-# File Server
 node ./bin/genieacs-fs &
-
-# NBI (API)
 node ./bin/genieacs-nbi &
-
-# UI (forçar porta do Render)
 node ./bin/genieacs-ui --port $PORT &
 
-# Aguardar todos os processos
 wait
