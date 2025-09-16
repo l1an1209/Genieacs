d4f9648fd80f7499eb1f6a5550391d0f6e8ad44b
Luan <luanpa082@gmail.com>
2025-09-16 02:39:45 -0300

    Adiciona start.sh e create-assets.js para deploy no Render

--- start.sh
+++ start.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-# Inicia todos os módulos do GenieACS usando PM2
+# Instala dependências e build
+npm install
+npm run build
 
-# CWMP (usa porta do Render)
-npx pm2 start bin/genieacs-cwmp --name genieacs-cwmp -- --port $PORT --log-level info
-
-# FS
-npx pm2 start bin/genieacs-fs --name genieacs-fs
-
-# NBI
-npx pm2 start bin/genieacs-nbi --name genieacs-nbi
-
-# Mantém logs ativos
-npx pm2 logs
+# Inicia o GenieACS CWMP ligado à porta do Render
+node bin/genieacs-cwmp --port $PORT --log-level info
