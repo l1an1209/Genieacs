68bc6034f664c4d75a5463cb6b82f4b3332da09e
Luan <luanpa082@gmail.com>
2025-09-16 22:09:28 -0300

    Configurar Redis Cloud no start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ mkdir -p logs
 
 # Defina suas URLs do MongoDB e Redis
 export MONGO_URL="mongodb+srv://luanpa082:L1an1010@cluster0.n3wzlxp.mongodb.net/genieacs?retryWrites=true&w=majority"
-export REDIS_URL="redis://localhost:6379" # ou URL do Redis na nuvem
+export REDIS_URL="rediss://default:GyF7WltUj6MDZ7xo7qTg568ppJUpzW38GyF7WltUj6MDZ7xo7qTg568ppJUpzW38@redis-14320.crce207.sa-east-1-2.ec2.redns.redis-cloud.com:14320"
 
 # Iniciar serviços com logs
 node ./bin/genieacs-cwmp &> logs/cwmp.log &
@@ -16,4 +16,3 @@ node ./bin/genieacs-ui   &> logs/ui.log   &
 
 # Esperar os processos terminarem
 wait
-
