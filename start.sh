eeaa5a06c7c02083574cf59b2f1509cdb286f14b
Luan <luanpa082@gmail.com>
2025-09-16 21:29:10 -0300

    Corrigido start.sh (criação da pasta logs)

--- start.sh
+++ start.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-# GenieACS start script para Linux/Render
+set -e
+
+# Criar pasta de logs se não existir
+mkdir -p logs
 
 # Defina suas URLs do MongoDB e Redis
 export MONGO_URL="mongodb+srv://luanpa082:L1an1010@cluster0.n3wzlxp.mongodb.net/genieacs?retryWrites=true&w=majority"
-export REDIS_URL="redis://localhost:6379" # ou sua URL do Redis
+export REDIS_URL="redis://localhost:6379" # ou URL do Redis na nuvem
 
-
-
+# Iniciar serviços com logs
 node ./bin/genieacs-cwmp &> logs/cwmp.log &
-node ./bin/genieacs-fs &> logs/fs.log &
-node ./bin/genieacs-nbi &> logs/nbi.log &
-node ./bin/genieacs-ui &> logs/ui.log &
+node ./bin/genieacs-fs   &> logs/fs.log   &
+node ./bin/genieacs-nbi  &> logs/nbi.log  &
+node ./bin/genieacs-ui   &> logs/ui.log   &
 
+# Esperar os processos terminarem
 wait
+
